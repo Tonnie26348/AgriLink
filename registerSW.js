a7bd68d7a5cca37bfc456da6ed05a1ce587b60c5
Tonnie26348 <Tonnie26348@users.noreply.github.com>
2026-05-01 12:36:39 +0000

    deploy: 458ae78f84e4490b87297e59f7b55d7be8889716

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/AgriLink/sw.js', { scope: '/AgriLink/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
